ec1bb4a1ad830fe24d846f86d64db898c5fa54ef
ThanK <hbthanh582002@gmail.com>
2024-12-20 02:56:01 +0700

    fix/deploy: Add some file

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,15 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 4444
+    port: 4444,
   },
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    commonjsOptions: { transformMixedEsModules: true },
+  },
 });
