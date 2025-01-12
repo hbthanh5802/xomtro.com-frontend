cd56cca1084180f35152b616f28bd3938592ea83
ThanK <hbthanh582002@gmail.com>
2025-01-12 10:27:59 +0700

    fix/comment: Fix comment attachment feature

--- src/components/PostAttachments/PassPostAttachment.tsx
+++ src/components/PostAttachments/PassPostAttachment.tsx
@@ -1,10 +1,10 @@
 import PostAttachmentItem from '@/components/PostAttachments/PostAttachmentItem';
-import PostCardSkeleton from '@/components/PostCardSkeleton';
+import PostAttachmentSkeleton from '@/components/PostAttachments/PostAttachmentSkeleton';
 import { PostTabProps } from '@/pages/UserPage/components/UserPostPage';
 import postService from '@/services/post.service';
 import { PostAttachmentType } from '@/store/postCommentSlice';
 import { useAppStore } from '@/store/store';
-import { Button, Divider, LinearProgress, Skeleton } from '@mui/joy';
+import { Button, Divider } from '@mui/joy';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { useShallow } from 'zustand/react/shallow';
 
@@ -48,7 +48,7 @@ export default function PassPostAttachment(props: PostTabProps) {
 
   return (
     <div className='tw-space-y-4'>
-      {!data && <PostCardSkeleton />}
+      {!data && <PostAttachmentSkeleton />}
       {data?.pages.map((page, index) => (
         <div key={index} className='tw-space-y-[18px]'>
           {page.results.map((post) => (
@@ -77,35 +77,8 @@ export default function PassPostAttachment(props: PostTabProps) {
           </Divider>
         </div>
       )}
-      {isFetching && (
-        <div className='tw-shadow-md tw-rounded-lg tw-bg-white tw-overflow-hidden tw-py-[24px]'>
-          <header className='tw-p-[14px] tw-pt-0 tw-flex tw-justify-between tw-items-center'>
-            <div className='tw-flex tw-items-center tw-gap-4'>
-              <Skeleton animation='wave' variant='circular' width={50} height={50} />
-              <div className='tw-space-y-2'>
-                <Skeleton animation='wave' variant='rectangular' width={200} height={18} />
-                <Skeleton animation='wave' variant='rectangular' width={100} height={16} />
-              </div>
-            </div>
-            <div className='tw-flex tw-items-center tw-gap-4'>
-              <Skeleton animation='wave' variant='rectangular' width={150} height={40} />
-            </div>
-          </header>
-          <LinearProgress variant='solid' color='neutral' size='sm' />
-          <main className='tw-mt-[24px]'>
-            <div className='tw-px-[14px]'>
-              <Skeleton animation='wave' variant='rectangular' width={400} height={24} />
-              <div className='tw-mt-4 tw-flex tw-items-center tw-gap-2'>
-                <Skeleton animation='wave' variant='rectangular' width={100} height={16} />
-                <Skeleton animation='wave' variant='rectangular' width={400} height={16} />
-              </div>
-              <div className='tw-mt-4 tw-flex tw-items-center tw-gap-2'>
-                <Skeleton animation='wave' variant='rectangular' sx={{ height: '300px' }} />
-              </div>
-            </div>
-          </main>
-        </div>
-      )}
+      {isFetching && <PostAttachmentSkeleton />}
+      {isFetching && <PostAttachmentSkeleton />}
     </div>
   );
 }
